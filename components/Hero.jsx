952ec64e9a301e5656b28ea0100814649b7db307
KChoi0720 <choicanlight@gmail.com>
2025-03-25 14:12:24 +1300

    adjust layout

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,8 +18,9 @@ import react from "react"
 
 
 const Hero = () => {
+    
     return (
-        <section className="py-12 xl:py-2 xl:pt-18\9\8 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+        <section className="py-12 xl:py-2 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
             <div className="container mx-auto">
                 <div className="flex justify-between gap-x-8">
                     <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">

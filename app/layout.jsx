8b5d1b3b3466644b4c42c8d29739e8af1a4d47d6
KChoi0720 <choicanlight@gmail.com>
2025-04-29 21:56:32 +1200

    use codes generated by google tag manager

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,25 +21,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' suppressHydrationWarning>
    
-      <head>
-        {/* GA4 Script */}
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
+   <Head>
+        {/* Google Tag Manager Script */}
+        <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PW46WDTW');`,
           }}
         />
-      </head>
+      </Head>
 
       <body className={outfit.className}>
         <Analytics />
